2252cfe212b68d760c2000b9492b3e5da960f16a
Magtimus <cristiancedano1030@gmail.com>
2022-03-17 16:10:04 -0400

    menu-hamburguesa-animado-con-CSS-y-JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
